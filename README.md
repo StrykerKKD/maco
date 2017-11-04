6165935eed71e201ea5b684e187c6362f234cbbd
stryker <kkdstryker@gmail.com>
2017-11-04 21:53:10 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# MACO: Virtual Machine in Ocaml
+
+MACO is a simple Ocaml port of the [MAC](https://github.com/felixangell/mac) project.
+
+## How to use:
+You should use the project with the latest version of the ocaml compiler.
+You can optain the latest version of ocaml with the use of the [opam switch command](https://opam.ocaml.org/doc/Usage.html#opam-switch).
+
+You only need jbuilder for this project:
+```sh
+opam install jbuilder
+```
+
+Clone:
+```sh
+git clone https://github.com/StrykerKKD/maco.git
+cd vmao
+```
+
+Build:
+```sh
+jbuilder build maco.exe
+```
+
+Run:
+```sh
+./_build/default/maco.exe
+```
---
+++ maco.ml
@@ -0,0 +1,57 @@
+let running = ref true
+let ip = ref 0
+let sp = ref (-1)
+
+let stack = Array.make 256 0
+
+type instruction_set =
+  | PSH
+  | ADD
+  | POP
+  | HLT
+  | Num of int
+
+let program = [|
+  PSH; Num 5;
+  PSH; Num 6;
+  ADD;
+  POP;
+  HLT 
+  |]
+
+let fetch () = program.(!ip)
+
+let get_number = function
+  | Num n -> n
+  | _ -> failwith "You can only push numbers onto the stack!"
+
+let rec eval = function
+  | HLT -> 
+    running := false;
+    Printf.printf "done\n"
+  | PSH ->
+    sp := !sp + 1;
+    ip := !ip + 1;
+    Array.set stack !sp (get_number program.(!ip))
+  | POP -> 
+    let val_popped = stack.(!sp) in
+    sp := !sp - 1;
+    Printf.printf "popped %d\n" val_popped
+  | ADD ->
+    let a = stack.(!sp) in
+    sp := !sp - 1;
+    let b = stack.(!sp) in
+    sp := !sp - 1;
+    let result = a + b in
+    sp := !sp + 1;
+    Array.set stack !sp result
+  | Num number ->
+    Printf.printf "number %d\n" number
+
+let main () =
+  while !running do
+    fetch () |> eval;
+    ip := !ip + 1
+  done
+
+let _ = main ()
